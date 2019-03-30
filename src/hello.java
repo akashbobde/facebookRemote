4114b11882dcdc6a3d5f12fba29091f4ff3d4e25
akashbobde <akashbobde1997@gmail.com>
2019-03-30 15:01:12 +0530

    my welcome commit

--- src/hello.java
+++ src/hello.java
@@ -3,8 +3,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("my forst commit");
-		
+		System.out.println("my first commit");
+	    System.out.println("welcome");	
 	}
 
 }
